2f6b823c3f0a4d3898cbc500f9bdedeebde43927
prakashujjwal <ujjwalprakash3@gmail.com>
2018-04-26 22:46:44 +0530

    added client.js

---
+++ client.js
@@ -0,0 +1,37 @@
+var socket = io.connect('http://localhost:4000');
+
+var message = document.getElementById('message');
+var myname = document.getElementById('myname');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+var popup = document.getElementById('popup');
+var online = document.getElementById('online');
+
+
+btn.addEventListener('click' , function(){
+  socket.emit('chatting' , {
+    message: message.value,
+    name: myname.value,
+  });
+});
+message.addEventListener('input',function(){
+  socket.emit('typing',myname.value);
+});
+
+online.addEventListener('load' , function(){
+  socket.emit('join' , myname.value);
+});
+
+
+socket.on('chatting',function(data){
+  popup.innerHTML = "";
+  output.innerHTML +='<p><strong>'+data.name+' :</strong>'+ ' '+data.message+'</p>';
+});
+
+socket.on('typing',function(data){
+  popup.innerHTML = '<p><i>'+data+' is typing a message'+'</i></p>';
+});
+
+socket.on('join',function(data){
+  online.innerHTML += '<p><b>'+data+' is online'+'</b></p>';
+});
